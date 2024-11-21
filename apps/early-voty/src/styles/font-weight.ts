cef20395815bd7c1d20ea9fd64f74a6f2a388856
yunshin <2younsin@gmail.com>
2024-11-21 22:57:33 +0900

    add: scss 라이브러리 설치, 디자인 토큰 설정

---
+++ apps/early-voty/src/styles/font-weight.ts
@@ -0,0 +1,11 @@
+export const FONT_WEIGHT = Object.freeze({
+  ex_thin: 100,
+  thin: 200,
+  semi_thin: 300,
+  regular: 400,
+  medium: 500,
+  semi_bold: 600,
+  bold: 700,
+  ex_bold: 800,
+  black: 900,
+} as const)
